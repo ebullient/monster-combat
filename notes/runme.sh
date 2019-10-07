445243e1866c35d8c5250aa30d1ffdaa5ffc10b6
Erin Schnabel <schnabel@us.ibm.com>
2019-10-07 12:29:53 -0400

    Allow URL to be specified

--- notes/runme.sh
+++ notes/runme.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+URL=$1
+if [ -z "$URL" ]; then
+  URL=http://monsters.192.168.99.100.nip.io/battle/faceoff
+fi
+
 while :
 do
-  curl http://monsters.192.168.99.100.nip.io/battle/faceoff
+  curl $URL
   sleep 3
 done
