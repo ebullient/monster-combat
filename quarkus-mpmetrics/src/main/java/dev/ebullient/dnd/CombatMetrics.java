9ac8f10af4077a86a3715dbc4cd93d107396d4a8
Ken Finnigan <ken@kenfinnigan.me>
2020-07-27 11:27:35 -0400

    MP Metrics version of service

---
+++ quarkus-mpmetrics/src/main/java/dev/ebullient/dnd/CombatMetrics.java
@@ -0,0 +1,74 @@
+package dev.ebullient.dnd;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.metrics.Gauge;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import org.eclipse.microprofile.metrics.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.ebullient.dnd.combat.Encounter;
+import dev.ebullient.dnd.combat.RoundResult;
+import dev.ebullient.dnd.mechanics.Dice;
+
+@ApplicationScoped
+public class CombatMetrics {
+    static final Logger logger = LoggerFactory.getLogger(dev.ebullient.dnd.combat.client.CombatMetrics.class);
+
+    final MetricRegistry registry;
+    final AtomicInteger last_roll = new AtomicInteger(0);
+    final AtomicInteger last_felled = new AtomicInteger(0);
+
+    public CombatMetrics(MetricRegistry registry) {
+        this.registry = registry;
+
+        registry.register("last.felled", (Gauge<AtomicInteger>) () -> last_felled);
+        registry.register("last.roll", (Gauge<AtomicInteger>) () -> last_roll);
+
+        Dice.setMonitor((k, v) -> {
+            registry.counter("dice.rolls", new Tag("die", k), new Tag("face", label(v))).inc();
+            last_roll.set(v);
+        });
+
+        logger.debug("Created CombatMetrics with MeterRegistry: {}", registry);
+    }
+
+    public void endEncounter(Encounter e, int totalRounds) {
+        registry.histogram("encounter.rounds",
+                new Tag("numCombatants", label(e.getNumCombatants())),
+                new Tag("targetSelector", e.getSelector()),
+                new Tag("sizeDelta", label(e.getSizeDelta())))
+                .update(totalRounds);
+    }
+
+    public void endRound(RoundResult result) {
+        for (RoundResult.Event event : result.getEvents()) {
+            registry.histogram("round.attacks",
+                    new Tag("hitOrMiss", event.hitOrMiss()),
+                    new Tag("attackType", event.getAttackType()),
+                    new Tag("damageType", event.getType()),
+                    new Tag("targetSelector", result.getSelector()))
+                    .update(event.getDamageAmount());
+
+            registry.histogram("attacker.damage",
+                    new Tag("attacker", event.getActor().getName()),
+                    new Tag("attackName", event.getName()),
+                    new Tag("hitOrMiss", event.hitOrMiss()))
+                    .update(event.getDamageAmount());
+
+            registry.histogram("attack.success",
+                    new Tag("attackType", event.getAttackType()),
+                    new Tag("hitOrMiss", event.hitOrMiss()))
+                    .update(event.getDifficultyClass());
+        }
+
+        last_felled.set(result.getNumCombatants() - result.getSurvivors().size());
+    }
+
+    String label(int value) {
+        return String.format("%02d", value);
+    }
+}
---
+++ quarkus-mpmetrics/src/main/java/dev/ebullient/dnd/EncounterResource.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright © 2020 IBM Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package dev.ebullient.dnd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+import dev.ebullient.dnd.bestiary.Bestiary;
+import dev.ebullient.dnd.combat.Encounter;
+import dev.ebullient.dnd.combat.RoundResult;
+import dev.ebullient.dnd.combat.TargetSelector;
+import dev.ebullient.dnd.mechanics.Dice;
+
+@Path("/combat")
+@Produces(MediaType.APPLICATION_JSON)
+public class EncounterResource {
+
+    final Bestiary beastiary;
+    final CombatMetrics metrics;
+
+    EncounterResource(BestiaryConfig config, CombatMetrics metrics) {
+        this.beastiary = config.getBestiary();
+        this.metrics = metrics;
+    }
+
+    @GET
+    @Path("/any")
+    @Timed
+    public List<RoundResult> any() {
+        return go(Dice.range(5) + 2);
+    }
+
+    @GET
+    @Path("/faceoff")
+    @Timed
+    public List<RoundResult> faceoff() {
+        return go(2);
+    }
+
+    @GET
+    @Path("/melee")
+    @Timed
+    public List<RoundResult> melee() {
+        return go(Dice.range(4) + 3);
+    }
+
+    List<RoundResult> go(int howMany) {
+
+        Encounter encounter = beastiary.buildEncounter()
+                .setHowMany(howMany)
+                .setTargetSelector(pickOne(howMany))
+                .build();
+
+        List<RoundResult> results = new ArrayList<>();
+
+        while (!encounter.isFinal()) {
+
+            RoundResult result = encounter.oneRound();
+            metrics.endRound(result);
+
+            results.add(result);
+        }
+        metrics.endEncounter(encounter, results.size());
+
+        return results;
+    }
+
+    TargetSelector pickOne(int howMany) {
+        int which = Dice.range(5);
+        switch (which) {
+            case 4:
+                return TargetSelector.SelectBiggest;
+            case 3:
+                return TargetSelector.SelectSmallest;
+            case 2:
+                return TargetSelector.SelectByHighestRelativeHealth;
+            case 1:
+                return TargetSelector.SelectByLowestRelativeHealth;
+            default:
+            case 0:
+                return TargetSelector.SelectAtRandom;
+        }
+    }
+}
