edb6f2f4e211e783c73463cd3cb7dd33ce7edec1
Erin Schnabel <ebullient@redhat.com>
2021-01-26 12:57:01 -0500

    scripts for kicking off requests

--- runme.sh
+++ runme.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Make sure we're running from the project root directory
+cd $BASEDIR
 
 case "$1" in
+  all-dc-micrometer)
+    nohup ./runme.sh dc-spring > spring.out &!
+    nohup ./runme.sh dc-quarkus > quarkus.out &!
+    nohup ./runme.sh dc-quarkus-native > quarkus-native.out &!
+    exit
+  ;;
+  all-dc-mpmetrics)
+    nohup ./runme.sh dc-mpmetrics > mppetrics.out &!
+    nohup ./runme.sh dc-mpmetrics-native > mppetrics-native.out &!
+    exit
+  ;;
   dc-spring)
     URL=http://localhost:8280/combat/any
   ;;
   dc-quarkus)
     URL=http://localhost:8281/combat/any
   ;;
-  dc-mpmetrics)
-    URL=http://localhost:8282/combat/any
-  ;;
   dc-quarkus-native)
     URL=http://localhost:8283/combat/any
+  ;;
+  dc-mpmetrics)
+    URL=http://localhost:8282/combat/any
   ;;
   dc-mpmetrics-native)
     URL=http://localhost:8284/combat/any
@@ -31,5 +46,6 @@ fi
 while :
 do
   curl $URL
-  sleep 20
+  echo "---"
+  sleep $[ ( $RANDOM % 10 ) + 1 ]s
 done
